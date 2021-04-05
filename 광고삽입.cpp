7a3bb6abcd657b8829a85f2dcc8a477561bce0f7
MoonGyu1 <78714820+MoonGyu1@users.noreply.github.com>
2021-04-05 20:23:03 +0900

    5주차-3단계/성공, prefix sum 알고리즘 사용

--- 광고삽입.cpp
+++ 광고삽입.cpp
@@ -1,126 +1,150 @@
 #include <string>
 #include <vector>
-#include <iostream>
-
 using namespace std;
 
-string solution(string play_time, string adv_time, vector<string> logs) {
-    string answer = "";
-    
-    if(play_time == adv_time){
-        answer = "00:00:00";
-        return answer;
-    }
-    
-    vector<int> logs_second_s(logs.size()); //logs 시작시간을 초단위로 변환
-    vector<int> logs_second_e(logs.size()); //logs 종료시간을 초단위로 변환
-    
-    for(int i=0; i<logs.size(); i++){
-        logs_second_s[i] = stoi(logs[i].substr(0,2)) * 60 * 60
-            + stoi(logs[i].substr(3,2)) * 60
-            + stoi(logs[i].substr(6,2));
-        
-        logs_second_e[i] = stoi(logs[i].substr(9,2)) * 60 * 60
-            + stoi(logs[i].substr(12,2)) * 60
-            + stoi(logs[i].substr(15,2));
-    }
-    
-    //각각의 log 시작시간에 광고가 시작한다고 가정
-    vector<int> accumulate(logs.size()); //각 시작시간 별 누적 재생시간
-    int adv_section_s; //실제 광고 시작 시간
-    int adv_section_e; //실제 광고 종료 시간
-    
-    int s; //start
-    int e; //end
-    
-    //adv_time을 초단위로 변환
-    int adv_time_second = stoi(adv_time.substr(0,2)) * 60 * 60
-            + stoi(adv_time.substr(3,2)) * 60
-            + stoi(adv_time.substr(6,2));
-    
-    for(int i=0; i<logs.size(); i++){
-        accumulate[i] = 0; //초기화
-        adv_section_s = logs_second_s[i]; //광고 시작 시간
-        adv_section_e = logs_second_s[i] + adv_time_second; //광고 종료 시간
-        
-        for(int j=0; j<logs.size(); j++){
-            if(adv_section_s <= logs_second_s[j]){
-                s = logs_second_s[j];
-            }
-            else{
-                s = adv_section_s;
-            }
-            
-            if(adv_section_e >= logs_second_e[j]){
-                e = logs_second_e[j];
-            }
-            else{
-                e = adv_section_e;
-            }
-            
-            if((e-s) > 0){
-                accumulate[i] += (e-s);
-            }
-        }  
-    }
-    
-    int answer_index = 0;
-    for(int i=1; i<accumulate.size(); i++){
-        if(accumulate[i] > accumulate[answer_index]){
-            answer_index = i;
-        }
-        else if(accumulate[i] == accumulate[answer_index]){
-            if(logs[i] < logs[answer_index]){
-                answer_index = i;
-            }
-        }
-    }
-    
-    answer = logs[answer_index].substr(0,8);
-//cout << answer_index <<endl;    
-    
-//     for(int i=0; i<logs.size(); i++){
-//         cout << accumulate[i] <<endl;
-//     }
-    
-    
-    return answer;
+//"00:00:00" -> second
+int string_to_int(string time) {
+	int second;
+	second = stoi(time.substr(0, 2)) * 60 * 60
+		+ stoi(time.substr(3, 2)) * 60
+		+ stoi(time.substr(6, 2));
+
+	return second;
 }
 
-//그리디?
-//구간별 누적재생시간을 구하고, 그 값이 가장 큰 위치부터 광고를 시작한다고 가정
+//second -> "00:00:00"
+string int_to_string(int second) {
+	string time = "";
 
-// 인풋1의 경우
-// 14:41 / 07:58 * 2 / 11:31 / 10:44 / 06:45 * 2 / 07:30 * 3 / 08:15 / 09:01 
-    
-// -> 구간 3개가 겹치는 07:30 * 3이 누적 재생 시간이 가장 긺
-// ->
+	int h = second / 60 / 60;
+	int m = second / 60 % 60;
+	int s = second % 60;
 
-// 1.logs 구간을 초단위로 바꾸기
-// 시간*60*60 + 분*60 +초
-// 4815 == 80분 15초 = 1시간 20분 15초
+	if (h < 10) { //한 자리수인 경우
+		time += "0";
+	}
+	time += to_string(h) + ":";
 
-// 광고가 시작하는 시간 = 항상 어떤 시청자의 재생시간의 시작시간과 일치한다고 가정
-// 1번 시청자의 시작시간부터 시작한다고 가정, 각각의 누적 재생시간을 구함
-// ex) 1번 부터 시작할 경우, 광고 시간이 00:14:15 이므로
-// 01:20:15 - 01:34:30 이라는 광고 구간을 얻을 수 있음
-// 이때의 누적 재생시간은
-// ---
-// 각 구간에서 시작할 경우 누적재생시간을 담는 vector[]
-// s,e //start, end
+	if (m < 10) {
+		time += "0";
+	}
+	time += to_string(m) + ":";
 
-// for문 1번 부터
-// if 광고 시작시간 <= i의 시작시간
-// s=i 시작시간
-// else
-// s= 광고 시작시간
+	if (s < 10) {
+		time += "0";
+	}
+	time += to_string(s);
 
-// ---
+	return time;
+}
 
-// if 광고 끝 시간 >= i의 끝시간
-// e= i 끝시간
-// else
-// e= 광고 끝시간
+string solution(string play_time, string adv_time, vector<string> logs) {
+	string answer = "";
 
-// e-s가 양수일 경우(음수면 구간이 겹치지 않는 것)
-// 1번 시작시간에 광고를 시작할 경우 누적재생시간 += e-s(양수일 경우)
+	//영상 길이 == 광고 길이
+	if (play_time == adv_time) {
+		answer = "00:00:00";
+		return answer;
+	}
+
+
+// 1. 주어진 영상 길이, 광고 길이, 재생 구간을 초단위로 변환
+
+	// play_time을 초단위로 변환
+	int play_second = string_to_int(play_time);
+
+	// adv_time을 초단위로 변환
+	int adv_second = string_to_int(adv_time);
+
+	// logs 시작시간, 종료시간을 초단위로 변환
+	vector<int> logs_start(logs.size());
+	vector<int> logs_end(logs.size());
+
+	for (int i = 0; i < logs.size(); i++) {
+		logs_start[i] = string_to_int(logs[i].substr(0, 8));
+		logs_end[i] = string_to_int(logs[i].substr(9, 8));
+	}
+
+
+// 2. 초당 시청자 수 구하기
+	int people[360000] = { 0, }; //초당 시청자 수 (최대 360000초)
+
+	for (int i = 0; i < logs.size(); i++) {
+		people[logs_start[i]]++; //시청 시작 시간에 +1
+		people[logs_end[i]]--; //시청 종료 시간에 -1
+	}
+
+	// people에 초당 누적 시청자 수 저장(prefix sum)
+	for (int i = 1; i < play_second; i++) {
+		people[i] = people[i - 1] + people[i];
+	}
+
+
+// 3. 누적 시청자 수가 가장 많은 구간 찾기
+	int start = 0; //광고 삽입 시각(초)
+
+	//testcase 17 에러: 자료형 int -> long
+	long now_sum = 0; //현재 누적재생시간
+	long max = 0; //최대 누적재생시간(초)
+
+	//now_sum과 max 초기화
+	//0초에 광고 삽입할 경우 누적재생시간(초)
+	for (int i = 0; i < adv_second; i++) {
+		now_sum += people[i];
+	}
+
+	max = now_sum;
+
+
+	//광고 삽입 구간을 1초씩 뒤로 이동하며 최댓값 찾기
+	for (int i = 0; i < play_second - adv_second; i++) {
+		now_sum -= people[i]; //맨 앞 요소 삭제
+		now_sum += people[i + adv_second]; //맨 뒤 요소 추가
+
+		if (now_sum > max) {
+			max = now_sum;
+			start = i + 1;
+		}
+	}
+
+
+// 4. 답을 시각으로 변환하여 반환
+	answer = int_to_string(start);
+
+	return answer;
+}
+
+
+
+
+//prefix sum 알고리즘을 사용해보자
+//-> 원하는 인덱스 구간의 '누적 합'을 구할 수 있음
+//전체 동영상 재생시간(최대 360000초)을 초단위로 환산
+// -> 1초마다 하나의 인덱스를 나타냄(인덱스: 0 ~ 360000)
+
+//play_time, adv_time을 초 단위로 반환
+//logs 시작/종료 시간을 초 단위로 반환
+
+//int people_num[360000]=0
+
+//초단위로 반환한 logs 요소를 탐색하면서
+//for i in logs시작시간
+// people_num[시작시간[i]] += 1
+
+//for i in logs종료시간
+// people_num[종료시간[i]] -= 1
+
+//현재까지 people_num배열에는 각 사람이 영상을 본 시작시간과 종료시간이 표시된 상태
+//->이것을 전구간에서 인원수로 바꿔주어야함
+
+//프리픽스 썸 알고리즘 사용
+//int people[360000]
+//people[i] = people[i-1] + people_num[i]
+
+//->people에는 각 초별 인원수가 담겨있음
+//->이제 사람 수가 가장 많은 구간을 뽑아야함
+//now_sum = 인덱스0~광고시간만큼(초단위)의 합
+//now_sum = now_sum - people[-1] + people[+1]
+//현재것이 더 크면 max= 현재것
+
+//return max
